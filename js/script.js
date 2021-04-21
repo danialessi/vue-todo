ecd3cdbd0fa15c3267cc4d33ecf3299e78d9e3e3
danialessi <daniele.alessi@hotmail.it>
2021-04-21 16:44:03 +0200

    aggiunta funzione per eliminare todo

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+
+var app = new Vue ({
+    el: '#root',
+    data: {
+        userInput: '',
+        todos: [
+            'fare i compiti',
+            'fare la spesa',
+            'fare il bucato'
+        ]
+    },
+    methods: {
+        addTodo() {
+            if (this.userInput.length > 0) {
+                this.todos.push(this.userInput);
+                this.userInput = '';
+            }
+        },
+        deleteToDo(index) {
+            this.todos.splice(index, 1)
+        }
+    }
+})
